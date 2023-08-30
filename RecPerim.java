b0ba2d15f7bf3d95c46c7b0deb267941a10db149
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-08-30 16:13:20 -0400

    Create RecPerim.java

---
+++ RecPerim.java
@@ -0,0 +1,19 @@
+/* RecPerim.java
+ * Sshahryar
+ * Calculates and displays the perimeter of a rectangle
+ */
+
+public class RecPerim  {
+    
+    public static void main(String[] args) {
+
+    int length = 13; //longer side of rectangle
+    int width = 4; //shorter side of rectangle
+    int perimeter; //calculated perimeter of rectangle                                  
+
+    perimeter = 2 * length + 2 * width;
+        
+    System.out.println("Perimeter of rectangle: " + perimeter);
+
+    }
+}
