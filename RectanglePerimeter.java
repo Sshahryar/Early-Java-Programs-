f6e67498a3923e326205be36eb9cacc4c9b337ad
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-08-31 18:28:05 -0400

    Create RectanglePerimeter.java

---
+++ RectanglePerimeter.java
@@ -0,0 +1,27 @@
+/* RectanglePerimeter.java
+ * Hussain Shahryar
+ * ICS-201 2a ch4a review 1
+ * Date: 2022-12-07
+ */
+
+
+/**
+* Calculates and displays the perimeter of a rectangle
+*/
+public class RectanglePerimeter  {
+    
+    public static void main(String[] args) {
+    
+    int length = 13; //longer side of rectangle
+    int width = 4; //shorter side of rectangle
+    int perimeter; //calculated perimeter of rectangle                                  
+
+    perimeter = 2 * length + 2 * width;
+        
+    System.out.println("Perimeter of rectangle;  " + perimeter); 
+        
+
+    System.out.println("Perimeter of rectangle:  " + perimeter);
+
+    }
+}
