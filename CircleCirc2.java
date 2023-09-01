29d10aa4e03db552ab22fe82a65dfb9b737c9894
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-08-31 21:36:41 -0400

    Create CircleCirc2.java

---
+++ CircleCirc2.java
@@ -0,0 +1,36 @@
+/* CircleCirc2.java
+ * Sshahryar
+ * Create a CircleCircumference application the calculates and displays the circumference of a circle. The application should prompt the user for the value of the radius. 
+ * The circumference of a circle is calculated as 2πr. 
+ * Use variables and constants as appropiate.
+ */
+
+import java.util.Scanner;
+
+public class CircleCirc2 {
+   
+   public static void main(String args[]){
+      
+      double radius;
+      double π;
+      double circumference;
+      
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.print("Enter the radius of the circle: ");
+      radius = sc.nextDouble();
+      
+      π = 3.14;
+      
+      circumference = 2*π*radius;
+      
+      if (radius <0) {
+         System.out.println( "Negative radii are illegal.");
+       } else if(radius >0){
+          System.out.println( "Circumference of the circle is: "+circumference);
+
+          sc.close();
+          
+      }
+   }
+}
