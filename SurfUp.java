8e220c61eb984987cc75f9e6f301a02c0f0e2ef8
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-09-02 20:40:59 -0400

    Create SurfUp.java

---
+++ SurfUp.java
@@ -0,0 +1,34 @@
+/* SurfUp.java
+* Sshahryar
+* Create a SurfsUp application that prompts the user for the wave height and then displays "Great day for surfing!" when the waves are 6 feet and over.
+*/
+
+import java.util.Scanner;
+
+public class SurfUp  {
+    
+    public static void main(String[] args)  {    
+        
+        int height;
+        
+        Scanner input = new Scanner(System.in);  
+
+        System.out.print("Enter wave height in feet: ");
+        height=input.nextInt(); 
+        input.close();
+
+        if (height >=78){
+            System.out.println( "Are you trying to break a record? The world record is 78 feet!"); 
+        } else if (height >=6){
+            System.out.println( "Great day for surfing!");
+        } else if(height>=3){
+            System.out.println( "Go body boarding!");
+        } else if(height>=0){
+            System.out.println( "Go for a swim.");
+        } else{
+            System.out.println( "Whoa! What kind of surf is that??");
+           
+        }
+    }
+}
+    
